c36cb3623b25cc8797dbd23e6ffa540a5dcc1457
Avinash Anchal <anchalavinash66@gmail.com>
2020-03-15 18:12:34 +0530

    template Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { CausesComponent } from './components/causes/causes.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'causes', component: CausesComponent},
+  {path: 'contactus', component: ContactComponent},
+  {path: 'aboutus', component: AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { CausesComponent } from './components/causes/causes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutusComponent,
+    ContactComponent,
+    BlogComponent,
+    CausesComponent
   ],
   imports: [
     BrowserModule,
