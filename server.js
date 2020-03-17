aa5008f1d4d79bc62155fe8d1d3829f381ab35ac
Avinash Anchal <anchalavinash66@gmail.com>
2020-03-17 09:45:28 +0530

    chnges

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+ 
+// const app = express();
+ 
+// // Serve only the static files form the angularapp directory
+// app.use(express.static(__dirname + '/develop-india2'));
+ 
+// app.get('/*', function(req,res) {
+ 
+// res.sendFile(path.join(__dirname+'/develop-india2/index.html'));
+// });
+ 
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/develop-india2'));
- 
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/develop-india'));
+
 app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/develop-india2/index.html'));
+    
+res.sendFile(path.join(__dirname+'/dist/develop-india/index.html'));
 });
- 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
