af657f0a92ad2f34e159f01b42fb097cae8bf531
Avinash Anchal <anchalavinash66@gmail.com>
2020-03-17 09:23:36 +0530

    minorchne

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/develop-india'));
+app.use(express.static(__dirname + '/develop-india2'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/develop-india/index.html'));
+res.sendFile(path.join(__dirname+'/develop-india2/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
