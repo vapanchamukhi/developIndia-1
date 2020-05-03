b4fffb26ff0b0624a289e12bf4b8c0a687311460
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-03 17:39:32 +0530

    Implemented new header

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.serice';
 
 @Component({
   selector: 'app-main-nav',
@@ -16,6 +17,42 @@ export class MainNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+    public authService: AuthService) {}
+
+    ngOnInit(){
+      if (localStorage.getItem("develop-login")) {
+        this.authService.loggedIn = true;
+      }
+    }
+
+    logout() {
+      this.authService.loggedIn = false;
+      localStorage.removeItem("develop-login");
+    }
+
+    createProfile() {
+      let form = {
+        email: 'vidya@developindia.com',
+        password: 'vidyadevelopindia'
+      }
+      let auths;
+      this.authService.getAuths().subscribe(data => {
+        auths = data
+        let authExists = false;
+        console.log('Recieved auths in header')
+        for (let auth of auths) {
+          if (auth.email === form.email && auth.password === form.password) {
+            authExists = true;
+            break;
+          }
+        }
+        if (!authExists) {
+          this.authService.onPostAuth(form);
+        }
+      },
+        error => console.log("error getting auths", error));
+  
+    }
 
 }
